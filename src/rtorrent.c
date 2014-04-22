168cb0731040f93046659dbbf2066758a903b9c1
Hsiun <hsiunkao@163.com>
2014-04-22 08:48:25 +0800

    modified:   src/rtorrent.c

--- src/rtorrent.c
+++ src/rtorrent.c
@@ -8,6 +8,8 @@
 #include "sha1.h"
 
 
+#define DEBUG
+
 char *torrent_content = NULL;
 long torrent_length = -1;
 
@@ -63,12 +65,164 @@ int readtorr(char *torrent){
 
 }
 
+int findkey(char *key, long *pos){
+    long i;
+
+    *pos = -1;
+    if (key == NULL){
+        return 0;
+    }
+    for (i=0; i<torrent_length-strlen(key); i++){
+        if(memcmp(&torrent_content[i], key, strlen(key)) == 0){
+            *pos = i;
+            return 1;
+        }
+    }
+
+    return 0;
+
+}
+
+int getann(){
+    annlist *node = NULL,*p = NULL;
+    int len = 0;
+    long i = 0;
+
+    if (findkey("13:announce-list",&i) == 0){                 //没有找到13:annouce-list
+        if (findkey("8:announce",&i) == 1) {
+            i = i + strlen("8:announce");
+            while (isdigit(torrent_content[i])){               //ctype所包含的库函数
+                len = len * 10 + ( torrent_content[i]-'0');    
+                i++;
+            }
+            i++;
+
+            node = (annlist *)malloc(sizeof(annlist));
+            strncpy(node->ann,&torrent_content[i],len);
+            node->ann[len] = '\0';
+            node->next = NULL;
+            annlist_head = node;
+
+        }
+    }
+    else{
+        i = i + strlen("13:announce-list");
+        i++;
+        while (torrent_content[i] != 'e'){
+            i++;
+            while (isdigit(torrent_content[i])){
+                len = len*10 + (torrent_content[i]-'0');
+                i++;
+            }
+            if(torrent_content[i]==':') i++;
+            else return -1;
+
+            if (memcmp(&torrent_content[i],"http",4) == 0){
+                node = (annlist *)malloc(sizeof(annlist));
+                strncpy(node->ann,&torrent_content[i],len);
+                node->ann[len] = '\0';
+                node->next = NULL;
+                if (annlist_head == NULL){
+                    node->next = NULL;
+                    annlist_head = node;
+
+                }
+                else {
+                    p = annlist_head;
+                    while ( p->next != NULL){
+                        p =  p->next;
+                    }
+                    p->next = node;
+
+                }
+            }
+            i = i + len;
+            len = 0;
+            i++;
+            if (i >= torrent_length)
+                return -1;
+
+        }
+    }
+
+
+#ifdef DEBUG
+    p = annlist_head;
+    while (p != NULL){
+        printf("%s\n",p->ann);
+        p = p->next;
+    }
+#endif
+    return 0;
+}
+
+int addann(char *url){
+    annlist *p,*q;
+    p = annlist_head;
+
+    while (p != NULL){
+        if(strcmp(p->ann,url) == 0)   //找到了和url相匹配的tarcker
+            break;
+        p = p -> next;
+    }
+
+    if (p != NULL)                    //如果p不为NULL，则说明url和已有的tracker重叠了
+        return 0;
+
+    q = (annlist *)malloc(sizeof(annlist)); //为放入annlist_head中url申请空间
+    strcpy(q->ann,url);
+    q->next = NULL;
+
+    p = annlist_head;
+    if (p == NULL){                   //如果annlist_head不存在，则将p赋予它
+        annlist_head = q;
+        return 1;
+    }
+    
+    while (p->next != NULL)           //存在则遍历到最后，将p加在annlist_head的后面
+        p = p->next;
+
+    p->next = q;
+
+    return 1;
+
+}
+
+int ismulti(){
+    long i;
+
+    if (findkey("5:files",&i) == 1){                //找到5:files关键字表面是多文件
+        ismultifile = 1;
+        return 1;
+    }
+
+#ifdef DEBUG
+    printf("is multi files:%d\n",ismultifile);     //此调试语句只在是单文件是有效
+#endif
+
+    return 0;
+}
+
+
+
 int rtorrent(char *torrent){
     int ret;
 
     ret = readtorr(torrent);
     if (ret < 0) {
-        printf("%s:%d wrong",__FILE__,__LINE__);
+        printf("%s:%d wrong:",__FILE__,__LINE__);
+        return -1;
+    }
+
+    ret = getann();
+    if (ret < 0) {
+        printf("%s:%d wrong:",__FILE__,__LINE__);
+        return -1;
+    }
+
+    ret = ismulti();
+    if (ret < 0) {
+        printf("%s:%d wrong:",__FILE__,__LINE__);
         return -1;
     }
 
